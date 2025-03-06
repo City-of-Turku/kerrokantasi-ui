9d52089d8e885e9d31567628e3dfe434aa5c24a0
Markus Hällfors <markus.hallfors@gofore.com>
2025-03-06 14:09:53 +0200

    Add openid_apitoken_url to settings optionalKeys

--- server/getSettings.js
+++ server/getSettings.js
@@ -63,6 +63,7 @@ const optionalKeys = [
   "openid_client_id",
   "openid_audience",
   "openid_authority",
+  "openid_apitoken_url",
   "enable_highcontrast",
   "admin_help_url",
   "enable_response_compression",
